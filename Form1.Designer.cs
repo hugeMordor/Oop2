fdf80c254de3dda4eac5ef873b413311900dbf97
hugeMordor <rykedfel@gmail.com>
2023-11-04 21:45:05 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿using System.Windows.Forms;
+
+namespace Oop2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 235);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Расчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(54, 45);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 29);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "2";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(54, 77);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 29);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "3";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(54, 110);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 29);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "4";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(54, 143);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 29);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.Text = "5";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(325, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 31);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(325, 75);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 31);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(325, 108);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(185, 31);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(325, 141);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(185, 31);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(198, 270);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(185, 31);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(198, 305);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(185, 31);
+            this.textBox6.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Евклид";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Стейн";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(388, 270);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(185, 31);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(388, 305);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(185, 31);
+            this.textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Enabled = false;
+            this.textBox9.Location = new System.Drawing.Point(325, 176);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(185, 31);
+            this.textBox9.TabIndex = 15;
+            this.textBox9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox9_KeyPress);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(588, 26);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(691, 713);
+            this.chart1.TabIndex = 16;
+            this.chart1.Text = "chart1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(92, 448);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(150, 29);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1494, 770);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private CheckBox checkBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Oop2
+{
+    public partial class Form1 : Form
+    {
+        bool isFirstTime = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetGcd getGcd = new GetGcd();
+            if (radioButton1.Checked 
+                && textBox1.Text != "" && textBox2.Text != "")
+            {
+                int a = Convert.ToInt32(textBox1.Text);
+                int b = Convert.ToInt32(textBox2.Text);
+                textBox5.Text = Convert.ToString(getGcd.GetGcdByEuclid(a, b, out long timeEuclid));
+                textBox6.Text = Convert.ToString(getGcd.GetGcdByStein(a, b, out long timeStein));
+                if (isFirstTime)
+                {
+                    textBox5.Text = Convert.ToString(getGcd.GetGcdByEuclid(a, b, out timeEuclid));
+                    textBox6.Text = Convert.ToString(getGcd.GetGcdByStein(a, b, out timeStein));
+                    isFirstTime = false;
+                }
+                textBox7.Text = Convert.ToString(timeEuclid);
+                textBox8.Text = Convert.ToString(timeStein);
+
+                DrawHistogram(getGcd.times);
+
+                textBox6.Enabled = true;
+                textBox7.Enabled = true;
+                textBox8.Enabled = true;
+            }
+            if (radioButton2.Checked 
+                && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                int a = Convert.ToInt32(textBox1.Text);
+                int b = Convert.ToInt32(textBox2.Text);
+                int c = Convert.ToInt32(textBox3.Text);
+                textBox5.Text = Convert.ToString(getGcd.GetGcdByEuclid(a, b, c));
+            }
+            if (radioButton3.Checked 
+                && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != ""
+                && textBox4.Text != "")
+            {
+                int a = Convert.ToInt32(textBox1.Text);
+                int b = Convert.ToInt32(textBox2.Text);
+                int c = Convert.ToInt32(textBox3.Text);
+                int d = Convert.ToInt32(textBox4.Text);
+                textBox5.Text = Convert.ToString(getGcd.GetGcdByEuclid(a, b, c, d));
+            }
+            if (radioButton4.Checked 
+                && textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != ""
+                && textBox4.Text != "" && textBox9.Text != "")
+            {
+                int a = Convert.ToInt32(textBox1.Text);
+                int b = Convert.ToInt32(textBox2.Text);
+                int c = Convert.ToInt32(textBox3.Text);
+                int d = Convert.ToInt32(textBox4.Text);
+                int f = Convert.ToInt32(textBox9.Text);
+                textBox5.Text = Convert.ToString(getGcd.GetGcdByEuclid(a, b, c, d, f));
+            }
+            textBox5.Enabled = true;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                textBox3.Enabled = true;
+            }
+            else
+            {
+                textBox3.Enabled = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+            }
+            else
+            {
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+                textBox9.Enabled = true;
+            }
+            else
+            {
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                textBox9.Enabled = false;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox9_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+        }
+        private void DrawHistogram(List<Tuple<int, int, long>> times, string orientation = "Vertical")
+        {
+            Axis axisX = new Axis();
+            Axis axisY = new Axis();
+
+            axisX.Title = "время, ticks";
+            axisY.Title = "Результат вычислений";
+
+            chart1.Series.Clear();
+            chart1.Series.Add("Число1");
+            chart1.Series.Add("Число2");
+
+            if (orientation == "Horizontal")
+            {
+                chart1.Series["Число1"].ChartType = SeriesChartType.Bar;
+                chart1.Series["Число2"].ChartType = SeriesChartType.Bar;
+            }
+
+            foreach (var item in times)
+            {
+                chart1.Series["Число1"].Points.AddXY(item.Item3, item.Item1);
+                chart1.Series["Число2"].Points.AddXY(item.Item3, item.Item2);
+            }
+
+            chart1.ChartAreas[0].Area3DStyle.Enable3D = false;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Font = new Font("Arial", 12);
+            chart1.ChartAreas[0].AxisY.LabelStyle.Font = new Font("Arial", 12);
+            chart1.ChartAreas[0].AxisY.LabelStyle.Format = "2";
+            chart1.ChartAreas[0].AxisX = axisX;
+            chart1.ChartAreas[0].AxisY = axisY;
+
+            chart1.Refresh();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class GetGcd
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        public List<Tuple<int, int, long>> times = new List<Tuple<int, int, long>>();
+        int count = 0;
+        public int GetGcdByEuclid(int a, int b, out long time)
+        {
+            stopwatch.Start();
+            int Gcd = GetGcdByEuclid(a, b);
+            stopwatch.Stop();
+            time = stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+            return Gcd;
+        }
+        public int GetGcdByEuclid(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if (a == b) return a;
+            if (a == 0) return b;
+            if (b == 0) return a;
+
+            while ((a != 0) && (b != 0))
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+            return Math.Max(a, b);
+        }
+        public int GetGcdByEuclid(int a, int b, int c)
+        {
+            return GetGcdByEuclid(GetGcdByEuclid(a, b), c);
+        }
+        public int GetGcdByEuclid(int a, int b, int c, int d)
+        {
+            return GetGcdByEuclid(GetGcdByEuclid(GetGcdByEuclid(a, b), c), d);
+        }
+        public int GetGcdByEuclid(int a, int b, int c, int d, int f)
+        {
+            return GetGcdByEuclid(GetGcdByEuclid(GetGcdByEuclid(GetGcdByEuclid(a, b), c), d), f);
+        }
+        public int GetGcdByStein(int a, int b, out long time)
+        {
+            stopwatch.Start();
+            int Gcd = GetGcdByStein(a, b);
+            stopwatch.Stop();
+            time = stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+            return Gcd;
+        }
+        private int GetGcdByStein(int a, int b)
+        {
+            stopwatch.Stop();
+            long time = stopwatch.ElapsedTicks;
+            if (count > 0)
+            {
+                times.Add(new Tuple<int, int, long>(a, b, time));
+            }
+            count++;
+            stopwatch.Reset();
+            stopwatch.Start();
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if (a == b) 
+            {
+                count = 0;
+                return a; 
+            }
+            if (a == 0)
+            {
+                count = 0;
+                return b;
+            }
+            if (b == 0)
+            {
+                count = 0;
+                return a;
+            }
+
+            if (a % 2 == 0)
+            {
+                if (b % 2 == 0) return 2 * GetGcdByStein(a / 2, b / 2);
+                return GetGcdByStein(a / 2, b);
+            }
+            if (b % 2 == 0) return GetGcdByStein(a, b / 2);
+            if (a > b) return GetGcdByStein((a - b) / 2, b);
+            return GetGcdByStein(a, (b - a) / 2);
+        }
+    }
+}
